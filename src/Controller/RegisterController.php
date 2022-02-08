db5f600a7471cbfa41f218070ec197179810874d
Fenosoa Andrianirina <fensooandrian@gmail.com>
2022-02-08 15:04:17 +0200

    Creation of the register form

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/inscription", name="register")
+     */
+    public function index(Request $request): Response
+    {
+
+        $user = new User();
+
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->persist($user);
+            $this->em->flush();
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
